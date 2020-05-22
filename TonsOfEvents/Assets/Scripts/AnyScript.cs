44e2680585b4056314f93378f79a23c6ede5de66
Isa <zhanbolot_i@auca.kg>
2020-05-22 23:40:56 +0600

    Start Test: - Event, Delegate, ChangeColor;

---
+++ TonsOfEvents/Assets/Scripts/AnyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool changed = false;
+    void Start()
+    {
+
+        //subscribe methods ChangeColor for event onClick
+        Main.test += ChangeColor;
+    }
+
+    //Method called each time events detected
+    public void ChangeColor() {
+        if (changed) {
+            GetComponent<MeshRenderer>().material.color = Color.white;
+            changed = false;
+        } else {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            changed = true;
+        }
+    }
+    // Update is called once per frame
+    
+}
---
+++ TonsOfEvents/Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public delegate void ActionClick();
+    public static event ActionClick onClick;
+    public delegate void Test();
+    public static event Test test;
+    // Start is called before the first frame update
+    public void ButtonClick() {
+        if (onClick != null) {
+            onClick();
+        }
+        if (test != null) {
+            test();
+        }
+    }
+
+    // Update is called once per frame
+
+}
