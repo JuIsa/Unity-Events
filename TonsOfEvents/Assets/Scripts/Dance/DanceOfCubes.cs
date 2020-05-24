3dea9a505e72f86da36da61e05986f9ac7033836
Isa <zhanbolot_i@auca.kg>
2020-05-25 02:11:20 +0600

    Sphere Dance: - Individual Scripts; - 1096 darwcall;

---
+++ TonsOfEvents/Assets/Scripts/Dance/DanceOfCubes.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceOfCubes : MonoBehaviour
+{
+    public GameObject pref;
+    public Material _mat;
+
+    private static List<List<GameObject>> cubes = new List<List<GameObject>>();
+    
+    void Start()
+    {
+        
+        float x = -10;
+        
+        for(int i = 0; i < 20; i++) {
+            float z = -10;
+            List<GameObject> list1 = new List<GameObject>();
+            for (int j = 0; j < 20; j++) {
+                GameObject go = Instantiate(pref, new Vector3(x, 0, z), Quaternion.identity);
+                go.GetComponent<MeshRenderer>().material = _mat;
+                
+                list1.Add(go);
+                
+                z+=1.0f;
+            }
+            cubes.Add(list1);
+            x+=1.0f;
+        }
+
+        StartCoroutine(shiftEverything());
+    }
+
+    IEnumerator shiftEverything() {
+        //Vector3 x = plus;
+        while (true) {
+            
+            for(int i = 0; i < 20; i++) {
+                for(int j = 0; j < 20; j++) {
+                    SelfAware sa = cubes[i][j].GetComponent<SelfAware>();
+                    sa.setPos(1, 1);
+                    Debug.Log(i+" and "+j);
+                    yield return new WaitForSeconds(0.05f);
+                }
+                
+            }
+            yield break;
+        }
+        
+    }
+
+
+
+
+   
+   
+}
---
+++ TonsOfEvents/Assets/Scripts/Dance/SelfAware.cs
@@ -0,0 +1,37 @@
+﻿using Packages.Rider.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfAware : MonoBehaviour
+{
+    private float x;
+    private float z;
+    private Vector3 plus = new Vector3(.0f, .5f, .0f);
+    private Vector3 minus = new Vector3(.0f, -.5f, .0f);
+    void Start()
+    {
+        
+    }
+
+    public void setPos(float posX, float posZ) {
+        x = posX;
+        z = posZ;
+        StartCoroutine(Dance());
+    }
+
+    IEnumerator Dance() {
+        Vector3 zz = plus;
+        while (true) {
+            if (this.transform.position.y > 2) {
+                zz = minus;
+            } else if (this.transform.position.y < -1) {
+                zz = plus;
+            }
+            this.transform.position += zz;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    
+    
+}
