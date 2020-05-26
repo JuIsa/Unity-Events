8bd96ac925e57cc435439d01870927b45809c7d5
Isa <zhanbolot_i@auca.kg>
2020-05-26 16:09:39 +0600

    Crappy Dissapear: - crappy fog of war;

---
+++ TonsOfEvents/Assets/Scripts/Dissapear/CreateCubes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCubes : MonoBehaviour
+{
+    public GameObject cubes;
+    void Start()
+    {
+        
+        float z = -10;
+        for(int i = 0; i < 20; i++) {
+            float x = -10;
+            for (int j = 0; j < 20; j++) {
+                Instantiate(cubes, new Vector3(x, 0, z), Quaternion.identity);
+                
+                x++;
+            }
+            z++;
+        }
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.A)) {
+            this.transform.position += Vector3.left * Time.deltaTime*2;
+        }else if (Input.GetKey(KeyCode.D)) {
+            this.transform.position += Vector3.right * Time.deltaTime*2;
+        } else if (Input.GetKey(KeyCode.W)) {
+            this.transform.position += Vector3.forward * Time.deltaTime * 2;
+        } else if (Input.GetKey(KeyCode.S)) {
+            this.transform.position += Vector3.back * Time.deltaTime * 2;
+        }
+    }
+
+
+}
---
+++ TonsOfEvents/Assets/Scripts/Dissapear/DissapearSelf.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissapearSelf : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Sphere");
+        StartCoroutine(checkDistance());
+    }
+
+    IEnumerator checkDistance() {
+        while (true) {
+            float distance = Vector3.Distance(this.transform.position, player.transform.position);
+            if (distance < 2.5) {
+                this.GetComponent<MeshRenderer>().enabled = false;
+            } else {
+                this.GetComponent<MeshRenderer>().enabled = true;
+            }
+            yield return new WaitForSeconds(0.1f);
+
+        }
+    }
+  
+}
