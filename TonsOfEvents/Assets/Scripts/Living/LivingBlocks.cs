1fedde9d5d702bf2227e801dfad943887c730864
Isa <zhanbolot_i@auca.kg>
2020-05-26 19:02:56 +0600

    Living Blocks: - Scripts - Prefab - Material

---
+++ TonsOfEvents/Assets/Scripts/Living/LivingBlocks.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingBlocks : MonoBehaviour
+{
+    public GameObject player;
+    public Material _upMaterial;
+    public Material _halfMaterail;
+    public float livingSpeed ;
+    private Material _defaultMaterial;
+    private MeshRenderer renderer;
+    private float x;
+    private float z;
+    private Vector3 target;
+    private Vector3 half;
+    private Vector3 basic;
+    void Start()
+    {
+        player = GameObject.Find("Sphere");
+        x = this.transform.position.x;
+        z = this.transform.position.z;
+
+        target = new Vector3(transform.position.x, 1.1f, transform.position.z);
+        half = new Vector3(transform.position.x, -.1f, transform.position.z);
+        basic = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        renderer = this.GetComponent<MeshRenderer>();
+        _defaultMaterial = renderer.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x1 = player.transform.position.x;
+        float z1 = player.transform.position.z;
+
+        float distanceX = Math.Abs(x - x1);
+        float distanceZ = Math.Abs(z - z1);
+        if (distanceX < 2 && distanceZ < 2) {
+            this.transform.position = Vector3.MoveTowards(transform.position, target, livingSpeed);
+            renderer.material = _upMaterial;
+        }
+        else if (distanceX < 3 && distanceZ < 3) {
+            this.transform.position = Vector3.MoveTowards(transform.position, half, livingSpeed);
+            renderer.material = _halfMaterail;
+        }
+        else {
+            this.transform.position = Vector3.MoveTowards(transform.position, basic, livingSpeed);
+            renderer.material = _defaultMaterial;
+        }
+
+    }
+}
---
+++ TonsOfEvents/Assets/Scripts/Living/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject cube;
+    public int grid;
+    public float speed;
+
+
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        float z = -grid/2;
+        for (int i = 0; i < grid; i++) {
+            float x = -grid/2;
+            for (int j = 0; j < grid; j++) {
+                Instantiate(cube, new Vector3(x, -2, z), Quaternion.identity);
+                x++;
+            }
+            z++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            rb.AddForce(Vector3.forward * speed);
+        } else if (Input.GetKey(KeyCode.S)) {
+            rb.AddForce(Vector3.back * speed);
+        } else if (Input.GetKey(KeyCode.A)) {
+            rb.AddForce(Vector3.left * speed);
+        } else if (Input.GetKey(KeyCode.D)) {
+            rb.AddForce(Vector3.right * speed);
+        }
+    }
+}
