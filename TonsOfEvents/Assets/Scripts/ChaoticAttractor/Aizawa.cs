10c796a0014abc263332d0904a46641681b65047
Isa <zhanbolot_i@auca.kg>
2020-05-28 02:09:45 +0600

    Aizawa Attractor: - enum for Aizawa, Lorenz

--- TonsOfEvents/Assets/Scripts/ChaoticAttractor/Aizawa.cs
+++ TonsOfEvents/Assets/Scripts/ChaoticAttractor/Aizawa.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Aizawa : MonoBehaviour
 {
 
-    public float time; 
+    public double time;
+
+    public enum AttractorOf { Aizawa, Lorenz};
+
+    public AttractorOf chooseAttractor;
 
     private ParticleSystem ps;
     
     private ParticleSystem.Particle[] _particles;
     private int alive;
 
-    private double a = 10;
-    private double b = 28;
-    private double c = 8/3;
-    private double d = 2f;
-    private double e = 0.25f;
-    private double f = 0.1f;
+    private double a = 0.95;
+    private double b = 0.7;
+    private double c = 0.6;
+    private double d = 2;
+    private double e = 0.25;
+    private double f = 0.1;
 
     double x ;
     double y;
@@ -29,8 +34,22 @@ public class Aizawa : MonoBehaviour
     double dz;
     void Start()
     {
+        //set x = 0.1 , y,z = 0
         ps = GetComponent<ParticleSystem>();
-        
+        if(chooseAttractor == AttractorOf.Lorenz) {
+            a = 10;
+            b = 28;
+            c = 8/3;
+        }
+        else if(chooseAttractor == AttractorOf.Aizawa) {
+            a = 0.95;
+            b = 0.7;
+            c = 0.6;
+            d = 3.5;
+            e = 0.25;
+            f = 0.1;
+            time = 0.01f;
+        }
         
     }
 
@@ -39,30 +58,40 @@ public class Aizawa : MonoBehaviour
     {
         _particles = new ParticleSystem.Particle[ps.main.maxParticles];
         alive = ps.GetParticles(_particles);
-        
-        for(int i = 0; i < alive; i++) {
-            x = _particles[i].position.x;
-            y =  _particles[i].position.y;
-            z =  _particles[i].position.z;
-            /* Aizawa
-            dx = ((z - b) * x - d * y)*time;
-            dy = (d * x + (z - b) * y)*time;
-            dz = (c + (a * z) -(( z * z * z) / 3) - (x * x) + f * z * (x * x * x))*time;    
-            
-            */
 
-            //Lorenz
-            dx = x + a * (y - x) * time;
-            dy = y + (x * (b - z) - y ) * time;
-            dz = z + (x * y - c * z) * time;
-            if (i == 1) {
-                Debug.Log(dx+" "+dy + " "+dz);
+        if (chooseAttractor == AttractorOf.Aizawa) {
+            for (int i = 0; i < alive; i++) {
+                x = _particles[i].position.x;
+                y = _particles[i].position.y;
+                z = _particles[i].position.z;
+
+                dx = (((z - b) * x) - (d * y)) * time;
+                dy = ((d * x) + ((z - b) * y)) * time;
+        //        dz = (c + (a * z) - (Math.Pow(z,3) / 3) - (Math.Pow(x,2)) + (Math.Pow(y, 2)) *(1 + e * z)+ f * z * (Math.Pow(x, 3))) * time;
+                dz = c + a * z - (Math.Pow(z, 3) / 3) - (Math.Pow(x, 2) + Math.Pow(y, 2)) * (1 + e * z) + f * z * (Math.Pow(x, 3));
+                dz *= time;
+                
+                float xx = Convert.ToSingle(dx);
+                float yy = Convert.ToSingle(dy);
+                float zz = Convert.ToSingle(dz);
+                _particles[i].position += new Vector3(xx, yy, zz);
             }
-            float xx = Convert.ToSingle(dx);
-            float yy = Convert.ToSingle(dy);
-            float zz = Convert.ToSingle(dz);
-            _particles[i].position = new Vector3(xx, yy, zz);
-            //_particles[i].position = Vector3.Lerp(_particles[i].position, new Vector3(xx, yy, zz), time);
+        } 
+        else if (chooseAttractor == AttractorOf.Lorenz) {
+            for (int i = 0; i < alive; i++) {
+                x = _particles[i].position.x;
+                y = _particles[i].position.y;
+                z = _particles[i].position.z;
+               
+                dx = x + a * (y - x) * time;
+                dy = y + (x * (b - z) - y) * time;
+                dz = z + (x * y - c * z) * time;
+                
+                float xx = Convert.ToSingle(dx);
+                float yy = Convert.ToSingle(dy);
+                float zz = Convert.ToSingle(dz);
+                _particles[i].position = new Vector3(xx, yy, zz);
+            }
         }
 
         ps.SetParticles(_particles,alive);
