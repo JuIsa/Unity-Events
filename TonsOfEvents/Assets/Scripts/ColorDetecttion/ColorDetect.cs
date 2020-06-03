03371b6ad10414a5e29a66aa8023ac74bd7d2940
Isa <zhanbolot_i@auca.kg>
2020-06-04 01:23:49 +0600

    CBG: - Extract exact rgb value; - Prefab size;

--- TonsOfEvents/Assets/Scripts/ColorDetecttion/ColorDetect.cs
+++ TonsOfEvents/Assets/Scripts/ColorDetecttion/ColorDetect.cs
@@ -9,8 +9,8 @@ public class ColorDetect : MonoBehaviour
     {
         MeshCollider mc = GetComponent<MeshCollider>();
 
-        Vector3 start = new Vector3(20.0f, .0f, .0f);
-        Vector3 target = new Vector3(-100.0f, .0f, .0f);
+        Vector3 start = new Vector3(2.0f, 0.1f, 0.0f);
+        Vector3 target = new Vector3(-2.0f,.0f,.0f);
 
         Debug.DrawRay(start,target, Color.red, 15f);
         RaycastHit objectHit;
@@ -20,6 +20,18 @@ public class ColorDetect : MonoBehaviour
             int index = mesh.triangles[objectHit.triangleIndex * 3];
             int ColorIndex = (int)(mesh.uv[index].x * 16);
             Debug.Log(ColorIndex);
+            //
+            Renderer renderer = objectHit.collider.GetComponent<MeshRenderer>();
+            Texture2D texture2D = renderer.material.mainTexture as Texture2D;
+            Vector2 pCoord = objectHit.textureCoord;
+            pCoord.x *= texture2D.width;
+            pCoord.y *= texture2D.height;
+
+            Vector2 tiling = renderer.material.mainTextureScale;
+            Color color = texture2D.GetPixel(Mathf.FloorToInt(pCoord.x * tiling.x), Mathf.FloorToInt(pCoord.y * tiling.y));
+            Debug.Log(color);
+
+
         }
     }
 
